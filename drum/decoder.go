de0d652ebdda290e40ec721eaefb30b03cae0988
David Pierce <david.dean.pierce@gmail.com>
2015-03-09 23:01:00 -0700

    Adds model for tracks and documents binary pattern

--- drum/decoder.go
+++ drum/decoder.go
@@ -9,7 +9,32 @@ func DecodeFile(path string) (*Pattern, error) {
 	return p, nil
 }
 
+// Pattern description
+//    offset - length  - value
+//     0        6      - "SPLICE"
+//     7        5      - padding
+//    13        1      - ???
+//    14       32      - HW Name
+//    46        4      - BPM (Little Endian Int32)
+// -- loop --
+//    0         1      - pattern (Hex -> Int)
+//    4         3      - padding
+//    8         1      - name length
+//    9         n      - pattern name
+//    9 + n    16      - pattern (01 for note, 00 for none)
+
+// Track is each line representing the sample and which
+// beats the sample should be played on
+type Track struct {
+	SampleID   int
+	SampleName string
+	Pattern    []byte
+}
+
 // Pattern is the high level representation of the
 // drum pattern contained in a .splice file.
-// TODO: implement
-type Pattern struct{}
+type Pattern struct {
+	HWVersion string
+	BPM       float32
+	Tracks    []Track
+}
